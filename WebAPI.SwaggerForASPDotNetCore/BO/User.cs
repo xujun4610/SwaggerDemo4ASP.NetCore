2846eb577c654671e517f750fca9b79df2f97125
haizhou940918 <xujun4610@sina.com>
2018-06-13 09:17:03 +0800

    build solution

---
+++ WebAPI.SwaggerForASPDotNetCore/BO/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.SwaggerForASPDotNetCore.BO
+{
+    public class User
+    {
+        public string AccountName { get; set; }
+        public string OSName { get; set; }
+        public int RAM_MB { get; set; }
+        public string CPUName { get; set; }
+        public float CPUClock { get; set; }
+        public bool IsEnabledJavaScript { get; set; }
+
+    }
+}
